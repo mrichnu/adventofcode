0f85daf2887689ff09eb6053fb8a9e6e2c9eb3ce
Matthew Rich <m-rich@northwestern.edu>
2015-12-21 10:40:54 -0600

    day 12: add filter

--- day12/index.js
+++ day12/index.js
@@ -1,12 +1,22 @@
 var input = require("./input.json");
 
-var parse_element = function(input) {
+var parse_element = function(input, skip) {
   var total = 0;
+  
+  // run through once to make sure "red" is not in input
+  if (skip !== undefined && typeof input === 'object' && !Array.isArray(input)) {
+    for (var e in input) {
+      if (e === skip || input[e] === skip) {
+        return total;
+      }
+    }
+  }
+
   for (var elem in input) {
     if (typeof input[elem] === 'number') {
       total += input[elem];
     } else if (typeof input[elem] === 'object') {
-      total += parse_element(input[elem]);
+      total += parse_element(input[elem], skip);
     }
   }
   
@@ -14,3 +24,5 @@ var parse_element = function(input) {
 }
 
 console.log("Total: " + parse_element(input));
+
+console.log("Total, skipping red: " + parse_element(input, "red"));
